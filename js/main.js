6faa36c9908e2de66fb5f133725554032d5ada84
Mindxx <quangcdwork@gmail.com>
2023-01-29 21:56:14 +0700

    push code to github

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Hover nav-item
+let hoverNavItem = () => {
+    let navItems = document.getElementsByClassName("nav-item")
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener("mouseover", () => {navItems[i].style.backgroundColor = "#e1603a"
+                                                         navItems[i].style.borderRadius = "10px" 
+                                                        })
+        navItems[i].addEventListener("mouseout", () => navItems[i].style.backgroundColor = "transparent")
+
+    }
+
+}
+
+hoverNavItem()
